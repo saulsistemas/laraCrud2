9a0bde5de75d9806406a4224bfa4eeb6ad36d178
Saul laravel <santamariaramos18@hotmail.com>
2021-12-26 11:58:59 -0500

    configuraciones

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+ 
+    public function index()
+    {
+        //
+    }
+
+ 
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(Categoria $categoria)
+    {
+        //
+    }
+
+ 
+    public function edit(Categoria $categoria)
+    {
+        //
+    }
+
+  
+    public function update(Request $request, Categoria $categoria)
+    {
+        //
+    }
+
+ 
+    public function destroy(Categoria $categoria)
+    {
+        //
+    }
+}
